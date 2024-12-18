4313f6e0e867f0b54872a669f23994a2236f415f
W. F. <we.f@posteo.net>
2024-12-18 14:49:21 +0100

    Remove dr_ones/src and dr_ones/topologies

--- dr_ones/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Library crate for the drone network simulator.
-//! Provides access to the core modules needed for testing and integration.
-
-// Public modules that can be used by tests and external crates
-pub mod client;
-pub mod drone;
-pub mod server;
-pub mod utils;
