ec4234169514b43398bc92c44c33eb91155134e0
W. F. <we.f@posteo.net>
2024-12-18 15:13:47 +0100

    Remove dr_ones/tests

--- dr_ones/tests/common/mod.rs
+++
@@ -1,6 +0,0 @@
-use std::fs;
-
-pub fn check_log_file(log_path: &str, expected: &[&str]) -> bool {
-    let log_content = fs::read_to_string(log_path).expect("Failed to read log file");
-    expected.iter().all(|&line| log_content.contains(line))
-}
