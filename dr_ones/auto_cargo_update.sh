b4f2c007d8d5497efcede234f799e9f9162475aa
FedeDes <federico.ds25@gmail.com>
2024-12-24 22:37:33 +0100

    cargo updated

---
+++ dr_ones/auto_cargo_update.sh
@@ -0,0 +1,16 @@
+cd client
+cargo update
+cd ../server
+cargo update
+cd ../host_node
+cargo update
+cd ../simulation_controller
+cargo update
+cd ../network_node
+cargo update
+cd ../drone
+cargo update
+cd ../network_initializer
+cargo update
+cd ..
+
